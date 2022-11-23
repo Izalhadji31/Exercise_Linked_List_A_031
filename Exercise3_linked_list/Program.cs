d640c894d37164bb22eed53a8aa33a4af3dc776e
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-11-23 15:34:10 +0700

    Add project files.

---
+++ Exercise3_linked_list/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Liked_List_A
+{
+    class Node
+    {
+        /*creates Nodes for the circular nexted list*/
+        {
+        }
+    }
+}
