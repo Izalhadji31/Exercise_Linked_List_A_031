7bfe9570670bf58487f8a2033a351141fdd44b72
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-11-23 15:41:36 +0700

    Menambahakan addNote

--- Exercise3_linked_list/Program.cs
+++ Exercise3_linked_list/Program.cs
@@ -9,7 +9,21 @@ namespace Exercise_Liked_List_A
     class Node
     {
         /*creates Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+    class CircularList
+    {
+        Node LAST;
+
+        public CircularList()
         {
+            LAST = null;
+        }
+        public void addNote()/*Method untuk menambahkan sebuah Node kedalam list*/
+        {
+          
         }
     }
 }
