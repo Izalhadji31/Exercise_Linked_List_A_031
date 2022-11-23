c2307b2b5ce398fa7a53ea369a3f54d502b2a19e
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-11-23 15:59:32 +0700

    Menambahkan delNode

--- Exercise3_linked_list/Program.cs
+++ Exercise3_linked_list/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exercise_Liked_List_A
@@ -15,15 +16,62 @@ namespace Exercise_Liked_List_A
     }
     class CircularList
     {
-        Node LAST;
+        Node START;
 
         public CircularList()
         {
-            LAST = null;
+            START = null;
         }
         public void addNote()/*Method untuk menambahkan sebuah Node kedalam list*/
         {
-          
+            int nim;
+            string nm;
+            Console.Write("\nMasukkan nomor Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan nama Mahasiswa: ");
+            nm= Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+            if (START == null || nim <= START.noMhs)
+            {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomor sama tidak diijinkan\n");
+                    return;
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+            }
+            /*Menemukan lokasi node baru di dalam list*/
+
+            Node previous, current;
+
+            previous = START;
+            current = START;
+
+            while ((current != null) && (nim >= current.noMhs))
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nNomor mahasiswa sama tidak diijinkan\n");
+                    return ;
+                }
+                previous = current;
+                current = current.next;
+            }
+            /*Node baru akan ditempatkan diantara previous dan current*/
+
+            nodeBaru.next = current;
+            previous.next = nodeBaru;
+        }
+
+        /*Method untuk menghapus node tertentu didalam list*/
+        public bool delNode(int nim)
+        {
+
         }
     }
 }
